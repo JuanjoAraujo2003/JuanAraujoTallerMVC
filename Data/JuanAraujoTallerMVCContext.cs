8923cb95604c77b6601f8f603b36ca616e9af08d
laboratorio <laboratorio@UPOAULA10622>
2023-10-11 09:14:48 -0500

    Finalizacion del taller

---
+++ Data/JuanAraujoTallerMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JuanAraujoTallerMVC.Models;
+
+namespace JuanAraujoTallerMVC.Data
+{
+    public class JuanAraujoTallerMVCContext : DbContext
+    {
+        public JuanAraujoTallerMVCContext (DbContextOptions<JuanAraujoTallerMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JuanAraujoTallerMVC.Models.Burger> Burger { get; set; } = default!;
+    }
+}
--- Models/Burger.cs
+++ Models/Burger.cs
@@ -6,9 +6,11 @@ namespace JuanAraujoTallerMVC.Models
     {
         public int Id { get; set; }
 
+
         [Required]
         public string? Name { get; set; }
         public bool WithCheese { get; set; }
+
 
         [Range(0.01, 9999.99)]
         public decimal Precio { get; set; }
